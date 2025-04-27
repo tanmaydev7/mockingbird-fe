036bfa95a59d26a16401b6902e1af5a18fd5f064
Tanmay070501 <xyzabc.am89@gmail.com>
2025-04-27 16:41:57 +0530

    fix: base in vite.config.js was the issue with deployment

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,19 @@
 import path from "path"
 import tailwindcss from "@tailwindcss/vite"
-import { defineConfig } from 'vite'
+import { defineConfig , loadEnv } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import svgr from 'vite-plugin-svgr'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react(), tailwindcss(), svgr()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
+export default defineConfig(({ mode }) => {
+  process.env = {...process.env, ...loadEnv(mode, process.cwd())};
+  return ({
+    plugins: [react(), tailwindcss(), svgr()],
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+      },
     },
-  },
+    base: process.env.VITE_BASE ?? "/"
+  })
 })
